91dba1dee1fdf6b94463af77ea6a886a4e7c3e03
getexi <getegakuen@gmail.com>
2023-03-18 00:06:12 +0900

    [add]1.cs | 1.csを実装

---
+++ algoMethod.cs/入力を受け取る/標準出力/1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.標準出力
+{
+    internal class _1
+    {
+        public void use_1()
+        {
+            Console.WriteLine("Hello Algo-method!");
+        }
+    }
+}
