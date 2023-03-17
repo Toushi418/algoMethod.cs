a0eec2e387dd9524503be3649053d41598c5518b
getexi <getegakuen@gmail.com>
2023-03-18 00:15:37 +0900

    [add]2.cs | 2.csを実装

---
+++ algoMethod.cs/入力を受け取る/標準出力/2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.標準出力
+{
+    internal class _2
+    {
+        public void use_2()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+                
+        }
+    }
+}
