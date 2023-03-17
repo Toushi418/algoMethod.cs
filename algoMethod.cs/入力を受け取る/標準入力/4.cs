1ede68e1d320eb21ee824bf5c7f9b8111f09561f
getexi <getegakuen@gmail.com>
2023-03-18 01:02:31 +0900

    [add]4.cs | 4.csを実装

---
+++ algoMethod.cs/入力を受け取る/標準入力/4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.標準入力
+{
+    internal class _4
+    {
+        public void use_4()
+        {
+            string S = Console.ReadLine();
+            Console.WriteLine(S[2]);
+        }
+    }
+}
