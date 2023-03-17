133aa202d11b5c90212c51ac55f1ab6856b02fed
getexi <getegakuen@gmail.com>
2023-03-18 02:05:39 +0900

    [add]4.cs | 4.csを実装

---
+++ algoMethod.cs/入力を受け取る/複数の入力を受け取る/4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.複数の入力を受け取る
+{
+    internal class _4
+    {
+        public void use_4()
+        {
+            var line = Console.ReadLine();
+            var l = line.Split(' ');
+            int A = int.Parse(l[0]);
+            int B = int.Parse(l[1]);
+
+            if (A % B == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
