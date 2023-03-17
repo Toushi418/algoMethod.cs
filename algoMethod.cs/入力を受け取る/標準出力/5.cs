826049ea2fd0416295e45f727b6ed219e0b5733a
getexi <getegakuen@gmail.com>
2023-03-18 00:16:13 +0900

    [add]5.js | 5.jsを実装

---
+++ algoMethod.cs/入力を受け取る/標準出力/5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.標準出力
+{
+    internal class _5
+    {
+        public void use_5()
+        {
+            int aDayTime = 24 * 60 * 60;
+            Console.WriteLine(aDayTime);
+        }
+    }
+}
