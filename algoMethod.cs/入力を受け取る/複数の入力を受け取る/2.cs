b12666e80cc42b689f6ab4ab195001e080e1a081
getexi <getegakuen@gmail.com>
2023-03-18 02:05:17 +0900

    [add]2.cs | 2.csを実装

---
+++ algoMethod.cs/入力を受け取る/複数の入力を受け取る/2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.複数の入力を受け取る
+{
+    internal class _2
+    {
+        public void use_2()
+        {
+            var line = Console.ReadLine();
+            var l = line.Split(' ');
+            var A = int.Parse(l[0]);
+            var B = int.Parse(l[1]);
+
+            if (A > B)
+            {
+                Console.WriteLine(A);
+            }
+            else
+            {
+                Console.WriteLine(B);
+            }
+        }
+    }
+}
