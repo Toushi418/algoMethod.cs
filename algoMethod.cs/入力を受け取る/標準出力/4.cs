53799a6a47bd1ee4ca70c0c557762e57948dab79
getexi <getegakuen@gmail.com>
2023-03-18 00:16:04 +0900

    [add]4.js | 4.jsを実装

---
+++ algoMethod.cs/入力を受け取る/標準出力/4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.標準出力
+{
+    internal class _4
+    {
+        public void use_4()
+        {
+            Console.WriteLine(314 * (159 + 265) - 358);
+        }
+    }
+}
