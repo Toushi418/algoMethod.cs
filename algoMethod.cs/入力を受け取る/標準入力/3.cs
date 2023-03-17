882b1bc0163186a0cd23c43e01b260d378f7cc92
getexi <getegakuen@gmail.com>
2023-03-18 01:02:15 +0900

    [add]3.cs | 3.csを実装

---
+++ algoMethod.cs/入力を受け取る/標準入力/3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.標準入力
+{
+    internal class _3
+    {
+        public void use_3()
+        {
+            string S = Console.ReadLine();
+            Console.WriteLine($"{S}" + $"{S}" + $"{S}");
+        }
+    }
+}
