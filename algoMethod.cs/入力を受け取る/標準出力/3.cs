31a7ed04830204247c9e181bbfcd0a3fe643bd31
getexi <getegakuen@gmail.com>
2023-03-18 00:15:57 +0900

    [add]3.js | 3.jsを実装

---
+++ algoMethod.cs/入力を受け取る/標準出力/3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.標準出力
+{
+    internal class _3
+    {
+        public void use_3()
+        {
+            Console.WriteLine(27182 % 818);
+        }
+    }
+}
