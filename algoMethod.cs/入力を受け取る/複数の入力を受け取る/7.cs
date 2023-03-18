be3fc5ddc993c65479f8fa4eebae67e819566352
getexi <getegakuen@gmail.com>
2023-03-18 19:49:47 +0900

    [add]7.cs | 7.csを実装

---
+++ algoMethod.cs/入力を受け取る/複数の入力を受け取る/7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.複数の入力を受け取る
+{
+    internal class _7
+    {
+        public void use_7()
+        {
+            var S = Console.ReadLine();
+            var T = Console.ReadLine();
+
+            if (S == T)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
