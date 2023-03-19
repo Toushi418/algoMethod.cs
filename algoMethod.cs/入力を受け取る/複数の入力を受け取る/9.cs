3f2d6b86a16627f01291a481780e5f1f3647f8f8
getexi <getegakuen@gmail.com>
2023-03-20 05:00:19 +0900

    [add]9.cs | 9.csを実装

---
+++ algoMethod.cs/入力を受け取る/複数の入力を受け取る/9.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.複数の入力を受け取る
+{
+    internal class _9
+    {
+        public void use_9()
+        {
+            var S = Console.ReadLine();
+            var N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(S[N - 1]);
+        }
+    }
+}
