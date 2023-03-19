30f6cd3ecbb3f0a640f51645210c9ba44cdc1a1a
getexi <getegakuen@gmail.com>
2023-03-20 05:00:08 +0900

    [add]8.cs | 8.csを実装

---
+++ algoMethod.cs/入力を受け取る/複数の入力を受け取る/8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.複数の入力を受け取る
+{
+    internal class _8
+    {
+        public void use_8()
+        {
+            string S = Console.ReadLine();
+            string T = Console.ReadLine();
+            string U = Console.ReadLine();
+
+            Console.WriteLine(U + T + S);
+        }
+    }
+}
