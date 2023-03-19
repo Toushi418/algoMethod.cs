8c61723d954bac0feac012c359f3af43eb2e8c36
getexi <getegakuen@gmail.com>
2023-03-20 05:00:30 +0900

    [add]10.cs | 10.csを実装

---
+++ algoMethod.cs/入力を受け取る/複数の入力を受け取る/10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.複数の入力を受け取る
+{
+    internal class _10
+    {
+        public void use_10()
+        {
+            var S = Console.ReadLine();
+            var NM = Console.ReadLine().Split(' ');
+            var N = int.Parse(NM[0]);
+            var M = int.Parse(NM[1]);
+
+            StringBuilder sb = new StringBuilder(S);
+            sb[N - 1] = S[M - 1];
+            sb[M - 1] = S[N - 1];
+
+            S = sb.ToString();
+            Console.WriteLine(S);
+        }
+    }
+}
