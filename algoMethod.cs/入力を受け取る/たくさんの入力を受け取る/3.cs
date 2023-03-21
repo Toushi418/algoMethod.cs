3489db0a42491c9eddb87659f18a1f385f3488d8
getexi <getegakuen@gmail.com>
2023-03-22 01:59:25 +0900

    [add]3.cs | 3.csを実装

---
+++ algoMethod.cs/入力を受け取る/たくさんの入力を受け取る/3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.たくさんの入力を受け取る
+{
+    internal class _3
+    {
+        public　void use_3()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var line = Console.ReadLine().Split(' ');
+            var numbers = line.Select(s => int.Parse(s)).ToArray();
+
+            for (var i = 0; i < N; i++)
+            {
+                Console.WriteLine(numbers[i] % 10);
+
+            }
+        }
+    }
+}
