105ffa870eb54b0d9a3403972a216374d3acaec9
getexi <getegakuen@gmail.com>
2023-03-22 01:52:59 +0900

    [add]2.js | 2.jsを実装

---
+++ algoMethod.cs/入力を受け取る/たくさんの入力を受け取る/1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoMethod.cs.入力を受け取る.たくさんの入力を受け取る
+{
+    internal class _1
+    {
+        public void use_1()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var numbers = Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray(); ;
+            int sum = 0;
+
+            for (var i = 0; i < N; i++)
+            {
+                sum += numbers[i];
+            }
+        }
+    }
+}
